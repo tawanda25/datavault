f33c1f52e039052636f27a0f0fe0b25492784883
tawanda25 <tawanda@datalab.co.za>
2023-10-20 10:38:17 +0200

    snowpipe config

---
+++ migrations/V1.1.6__snowpipe_config.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE PIPE stg_orders_pp 
+AS 
+COPY INTO stg_orders 
+FROM
+(
+SELECT $1,$2,$3,$4,$5,$6,$7,$8,$9 
+     , metadata$filename
+     , metadata$file_row_number
+     , CURRENT_TIMESTAMP()
+     , 'Orders System'
+  FROM @orders_data
+);
+
+CREATE OR REPLACE PIPE stg_customer_pp 
+--AUTO_INGEST = TRUE
+--aws_sns_topic = 'arn:aws:sns:mybucketdetails'
+AS 
+COPY INTO stg_customer
+FROM 
+(
+SELECT $1
+     , metadata$filename
+     , metadata$file_row_number
+     , CURRENT_TIMESTAMP()
+     , 'Customers System'
+  FROM @customer_data
+);
+
+ALTER PIPE stg_customer_pp REFRESH;
+
+ALTER PIPE stg_orders_pp   REFRESH;

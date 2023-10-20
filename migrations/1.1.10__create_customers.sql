e95a5c168fd84e2d8e83764f2334db9bd78f7ee2
tawanda25 <tawanda@datalab.co.za>
2023-10-20 12:06:34 +0200

    customers

---
+++ migrations/1.1.10__create_customers.sql
@@ -0,0 +1,10 @@
+use schema L00_STG;
+
+CREATE OR REPLACE TABLE stg_customers
+(
+  raw_json                VARIANT
+, filename                STRING   NOT NULL
+, file_row_seq            NUMBER   NOT NULL
+, ldts                    STRING   NOT NULL
+, rscr                    STRING   NOT NULL
+);

d679eead08b5f2de9599506a89cb16c51adf266d
tawanda25 <tawanda@datalab.co.za>
2023-10-20 10:30:52 +0200

    commit files

---
+++ migrations/V1.1.5__copy_files.sql
@@ -0,0 +1,13 @@
+COPY INTO @customer_data 
+FROM
+(SELECT object_construct(*)
+  FROM snowflake_sample_data.tpch_sf10.customer limit 10
+) 
+INCLUDE_QUERY_ID=TRUE;
+
+COPY INTO @orders_data 
+FROM
+(SELECT *
+  FROM snowflake_sample_data.tpch_sf10.orders limit 1000
+) 
+INCLUDE_QUERY_ID=TRUE;
